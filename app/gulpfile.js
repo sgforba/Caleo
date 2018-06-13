3c7aa415ad79d82c5015b9b195198ea358e2658d
Unknown <sgforba@gmail.com>
2018-06-13 15:04:31 -0400

    Wordpress Init

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -3,12 +3,12 @@ const sass = require('gulp-sass');
 
 //Compile Sass
 gulp.task('sass', function(){
-    gulp.src('./sass/*.scss')
+    gulp.src('./assets/sass/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./'))
 });
 
 //Watches Sass
 gulp.task('watch', function(){
-    gulp.watch('./sass/*.scss', ['sass']);
+    gulp.watch('./assets/sass/*.scss', ['sass']);
 })
